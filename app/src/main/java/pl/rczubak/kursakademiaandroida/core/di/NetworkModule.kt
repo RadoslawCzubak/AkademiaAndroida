39c79a2a8ac1e387a2d3d7b2f9658f5225a586a0
Radosław Czubak <56758365+RadoslawCzubak@users.noreply.github.com>
2022-02-15 18:50:39 +0100

    Module3 - Dependency Injection (#2)
    * Add network module.
    
    * Add Room dependencies and update navigation dependencies.
    
    * Add AppModule.
    
    * Add DatabaseModule.
    
    * Fix bug.

---
+++ app/src/main/java/pl/rczubak/kursakademiaandroida/core/di/NetworkModule.kt
@@ -0,0 +1,34 @@
+package pl.rczubak.kursakademiaandroida.core.di
+
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.module
+import pl.rczubak.kursakademiaandroida.BuildConfig
+import pl.rczubak.kursakademiaandroida.features.data.RickAndMortyApi
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+val networkModule = module {
+
+    single<Interceptor> {
+        HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY
+            else HttpLoggingInterceptor.Level.NONE
+        }
+    }
+
+    single {
+        OkHttpClient.Builder()
+            .addInterceptor(get<Interceptor>())
+    }
+
+    single {
+        Retrofit.Builder()
+            .client(get())
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://rickandmortyapi.com/api/")
+    }
+
+    single { get<Retrofit>().create(RickAndMortyApi::class.java) }
+}
---
+++ app/src/main/java/pl/rczubak/kursakademiaandroida/features/data/RickAndMortyDb.kt
@@ -0,0 +1,7 @@
+package pl.rczubak.kursakademiaandroida.features.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [], version = 1)
+abstract class RickAndMortyDb : RoomDatabase()
