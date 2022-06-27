2dda2e976301183d7035ea211db1a98caf0a90c3
RadoslawCzubak <radoslaw.marek.czubak@gmail.com>
2022-06-27 22:07:37 +0200

    add lesson 5

--- app/src/main/java/pl/rczubak/kursakademiaandroida/features/episodes/data/local/EpisodeDao.kt
+++ app/src/main/java/pl/rczubak/kursakademiaandroida/features/episodes/data/local/EpisodeDao.kt
@@ -1,7 +1,13 @@
 package pl.rczubak.kursakademiaandroida.features.episodes.data.local
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import pl.rczubak.kursakademiaandroida.features.episodes.data.local.model.EpisodeCached
 
 @Dao
 interface EpisodeDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveEpisodes(vararg episode: EpisodeCached)
 }
--- app/src/main/java/pl/rczubak/kursakademiaandroida/features/episodes/data/repository/EpisodeRepositoryImpl.kt
+++ app/src/main/java/pl/rczubak/kursakademiaandroida/features/episodes/data/repository/EpisodeRepositoryImpl.kt
@@ -1,15 +1,25 @@
 package pl.rczubak.kursakademiaandroida.features.episodes.data.repository
 
 import pl.rczubak.kursakademiaandroida.core.api.RickAndMortyApi
+import pl.rczubak.kursakademiaandroida.features.episodes.data.local.EpisodeDao
+import pl.rczubak.kursakademiaandroida.features.episodes.data.local.model.EpisodeCached
 import pl.rczubak.kursakademiaandroida.features.episodes.domain.EpisodeRepository
 import pl.rczubak.kursakademiaandroida.features.episodes.domain.model.Episode
 
 class EpisodeRepositoryImpl(
-    private val api: RickAndMortyApi
+    private val api: RickAndMortyApi,
+    private val episodeDao: EpisodeDao
 ) : EpisodeRepository {
     override suspend fun getEpisodes(): List<Episode> {
         return api.getEpisodes().results.map {
             it.toEpisode()
         }
+            .also { saveEpisodes(it) }
+    }
+
+    private suspend fun saveEpisodes(episodes: List<Episode>) {
+        episodes.map { EpisodeCached(it) }
+            .toTypedArray()
+            .let { episodeDao.saveEpisodes(*it) }
     }
 }
